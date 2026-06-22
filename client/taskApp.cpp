42870aa2d1d9ffdb7794c1a232ab6c8c6559f132
ruukha <ruukha3@gmail.com>
2026-06-22 16:37:28 +0200

    Modified TaskApp begin() to implement last commits

--- client/taskApp.cpp
+++ client/taskApp.cpp
@@ -4,18 +4,34 @@
 #include "pins.h"
 #include "button.h"
 #include "config.h"
+#include "encoder.h"
+#include "screen.h"
+#include "initHandler.h"
 
 LightSensor lightSensor;
 Button button{Pins::BTN, Config::DEBOUNCE_MS, Config::HOLD_MS};
 Button encoderButton{Pins::ENC_SW, Config::DEBOUNCE_MS, Config::HOLD_MS};
+Encoder encoder{Pins::ENC_DT, Pins::ENC_CLK};
+Screen screen{Pins::TFT_CS, Pins::TFT_RST, Pins::TFT_DC, Pins::TFT_SDI, Pins::TFT_SCK, Pins::TFT_LED};
+
+InitHandler initHandler(screen);
 
 void TaskApp::begin(){
-    lightSensor.begin();
+    initHandler.handle(screen.begin());
+    delay(100);
 
-    button.begin();
-    encoderButton.begin();
+    auto lightSensorResult = lightSensor.begin();
+    initHandler.handle(lightSensorResult);
+
+    initHandler.handle(button.begin());
+    initHandler.handle(encoderButton.begin());
+
+    initHandler.handle(encoder.begin());
+
+    screen.setTextSize(Config::TEXT_SIZE);
+    screen.clear();
+    delay(100);
 }
 
 void TaskApp::update(){
-    
 }
